fd0a11257448252f3b28b917d3750d0d7b060913
Hannah Gavrilova <anya94qwe@gmail.com>
2026-06-18 14:55:29 +0300

    chore: modify playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -29,7 +29,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // Репортеры (куда выводить результаты)
-  reporter: 'html',
+  reporter: [ ['html', { outputFolder: 'playwright-report' }]
+],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
